c602decfd82dd96e87fc1fa572d7039a6edbab98
irfanrmdni <irfanrmdni209@gmail.com>
2020-10-15 17:23:59 +0700

    menambahkan output pada latihan 1.cpp

--- latihan1.cpp
+++ latihan1.cpp
@@ -8,5 +8,7 @@ int main(int argc, char const *argv[])
     cout << "Membuat repo di GITHUB dan menghubungkannya ke GIT lokal dengan clone" << endl;
 
     cout << "hello word" << endl;
+
+    cout << "semangat" << endl;
     return 0;
 }
