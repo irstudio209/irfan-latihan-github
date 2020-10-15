106289ae24c3a72155c3241eb51fedd1c01e966e
irfanrmdni <irfanrmdni209@gmail.com>
2020-10-15 16:54:40 +0700

    membuat file baru latihan1.cpp

---
+++ latihan1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Latihan GIT" << endl;
+    cout << "Membuat repo di GITHUB dan menghubungkannya ke GIT lokal dengan clone" << endl;
+    return 0;
+}
